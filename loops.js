7f0c2a487fc13cfebaa7fd249f1ee159a93dfe2a
Clark Chen <yyunqingchenn@gmail.com>
2019-07-02 01:50:01 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -17,15 +17,16 @@ function whileLoop(n){
   return "done"
 }
 
-var i = 0;
+
+
+function doWhileLoop(num){
+  var i = 0;
  
 function incrementVariable() {
   i = i + 1;
   return i;
 }
-
-function doWhileLoop(num){
-  do{
+do{
     console.log("I run once regardless.");
   } while (incrementVariable() < num);
 }
